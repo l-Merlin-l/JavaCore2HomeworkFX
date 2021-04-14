0c7a996eb386c4769533473c2df76073d0ed18b9
merlin <oymerlinyo@gmail.com>
2021-04-14 23:24:30 +0300

    Lesson4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextArea Messages;
+    @FXML
+    private TextField Message;
+
+    public void clickBtn1(ActionEvent actionEvent) {
+        if (!Message.getText().equals("")) {
+            Messages.appendText("Вы пишите:\n" + Message.getText() + "\n");
+            Message.clear();
+            Message.requestFocus();
+        }
+    }
 }
